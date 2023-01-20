b831eb003afdb5db1e7a8bc1020ea9498b8214a1
황성수 <112155469+pkdie@users.noreply.github.com>
2023-01-20 16:38:18 +0900

    [Silver V] Title: 나이순 정렬, Time: 240 ms, Memory: 45488 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,9 +1,11 @@
-N = int(input())
+import sys
+
+N = int(sys.stdin.readline())
 member = []
 for _ in range(N):
-    age, name = map(str, input().split())
+    age, name = sys.stdin.readline().split()
     age = int(age)
     member.append((age, name))
-sorted_member = sorted(member, key=lambda x: x[0])
-for mem in sorted_member:
+member.sort(key= lambda x : x[0])
+for mem in member:
     print(mem[0], mem[1])
