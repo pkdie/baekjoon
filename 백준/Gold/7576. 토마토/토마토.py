19f3ef77aa72f4047b0a98c90ad8cae01cebbf1b
ss <112155469+pkdie@users.noreply.github.com>
2023-05-11 01:59:08 +0900

    [Gold V] Title: 토마토, Time: 1268 ms, Memory: 98536 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,41 +1,40 @@
-import sys
 from collections import deque
 
-M, N = map(int, sys.stdin.readline().split())
+m, n = map(int, input().split())
 
 graph = []
-for _ in range(N):
-    graph.append(list(map(int, sys.stdin.readline().split())))
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-queue = deque()
-
-dx = [1, -1, 0, 0]
+dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1:
-            queue.append((i, j))
+def bfs(graph):
+    queue = deque()
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                queue.append((i, j))
 
-day = 0
+    while queue:
+        x, y = queue.popleft()
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == -1:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
 
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or ny < 0 or nx >= N or ny >= M:
-            continue
-        if graph[nx][ny] == -1:
-            continue
-        if graph[nx][ny] == 0:
-            graph[nx][ny] = graph[x][y] + 1
-            queue.append((nx, ny))
+    max_value = 0
+    for idx in graph:
+        if 0 in idx:
+            return -1
+        if max(idx) > max_value:
+            max_value = max(idx)
+    return max_value - 1
 
-for i in graph:
-    if 0 in i:
-        print(-1)
-        exit(0)
-    day = max(day, max(i))
-
-print(day - 1)
+print(bfs(graph))
