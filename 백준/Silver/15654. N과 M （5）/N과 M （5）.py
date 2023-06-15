187f3a3983b59138aeb9614807af864e23c07d9e
ss <112155469+pkdie@users.noreply.github.com>
2023-06-15 14:49:57 +0900

    [Silver III] Title: N과 M (5), Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+seq = list(map(int, input().split()))
+seq.sort()
+ans = []
+
+def dfs():
+    if len(ans) == m:
+        print(" ".join(map(str, ans)))
+        return
+
+    for num in seq:
+        if num not in ans:
+            ans.append(num)
+            dfs()
+            ans.pop()
+
+dfs()
